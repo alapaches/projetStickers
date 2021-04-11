ac23554d11c1339bb0848979e4b792af7562a813
alapaches <juliensculteur@gmail.com>
2021-04-11 18:41:21 +0200

    fix images

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $(".img-sticker").each(function() {
+        var nom = $(this).data("nom");
+        $(this).popover({
+            container: "body",
+            trigger: "hover",
+            placement: "top",
+            html: true,
+            content: nom
+        });
+    })
+})
